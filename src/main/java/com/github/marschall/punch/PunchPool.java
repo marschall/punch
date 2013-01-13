5ca4c85b3317fa151a61a0e6a94218d29394f9fb
Philippe Marschall <philippe.marschall@gmail.com>
2013-01-13 22:43:57 +0100

    PunchPool has to be public

--- src/main/java/com/github/marschall/punch/PunchPool.java
+++ src/main/java/com/github/marschall/punch/PunchPool.java
@@ -3,13 +3,13 @@ package com.github.marschall.punch;
 import java.util.concurrent.ForkJoinPool;
 
 
-class PunchPool extends ForkJoinPool {
+public final class PunchPool extends ForkJoinPool {
 
   final TaskStateListener listener;
 
-  PunchPool(TaskStateListener listener) {
+  public PunchPool(TaskStateListener listener) {
     if (listener == null) {
-      throw new IllegalArgumentException("A task state listener is mandatory.");
+      throw new NullPointerException("A task state listener is mandatory.");
     }
     this.listener = listener;
   }
