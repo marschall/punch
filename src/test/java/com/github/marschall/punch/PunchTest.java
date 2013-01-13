6348fbd080e6a7c6996380e1f18446989e2c37a6
Stefan Ferstl <st.ferstl@gmail.com>
2013-01-13 22:58:37 +0100

    draft of persisting task states

--- src/test/java/com/github/marschall/punch/PunchTest.java
+++ src/test/java/com/github/marschall/punch/PunchTest.java
@@ -1,9 +1,6 @@
 package com.github.marschall.punch;
 
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.concurrent.ForkJoinPool;
-import java.util.concurrent.RecursiveAction;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.After;
@@ -29,7 +26,7 @@ public class PunchTest {
 
   @Test
   public void batchSample() {
-    this.pool.invoke(this.buildFileOutRoot());
+    this.pool.invoke(JobTrees.buildFileOutRoot());
   }
 
   /**
@@ -37,75 +34,7 @@ public class PunchTest {
    */
   @Test
   public void treeSample() {
-    this.pool.invoke(buildTree());
-  }
-
-  /**
-   * <pre>
-   * parallel
-   *   - sequential
-   *     - serial-1
-   *     - serial-2
-   *   - singleTask-1
-   *   - sequential
-   *     - serial-3
-   *     - serial-4
-   * </pre>
-   */
-  private RecursiveAction buildTree() {
-    RecoverableTask serial1 = buildSerialTasks(1, 2);
-    RecoverableTask singleTask = new StringTask("singleTask-1");
-    RecoverableTask serial2 = buildSerialTasks(3, 2);
-
-    Collection<RecoverableTask> tasks = new ArrayList<>(3);
-    tasks.add(serial1);
-    tasks.add(singleTask);
-    tasks.add(serial2);
-    return new ParallelTaskContainer(tasks);
-  }
-
-  private RecoverableTask buildSerialTasks(int start, int numberOfTasks) {
-    Collection<RecoverableTask> tasks = new ArrayList<>(numberOfTasks);
-    for (int i = start; i < start + numberOfTasks; i++) {
-      tasks.add(new StringTask("serial-" + i));
-    }
-    return new SerialTaskContainer(tasks);
-  }
-
-  /**
-   * <pre>
-   * fileout-root
-   *  - tenants (parallel)
-   *    - tenant1 (sequential)
-   *      - staging (parallel)
-   *        - staging-job1
-   *        - staging-job2
-   *        - ...
-   *      - writing-job
-   *    - tenant2 (sequential)
-   *      ...
-   * </pre>
-   */
-  private RecoverableTask buildFileOutRoot() {
-    Collection<RecoverableTask> tasks = new ArrayList<>(2);
-    tasks.add(buildTenantJobs(1, 10));
-    tasks.add(buildTenantJobs(2, 5));
-    return new ParallelTaskContainer(tasks);
-  }
-
-  private RecoverableTask buildTenantJobs(int tenant, int numberOfStagingJobs) {
-    Collection<RecoverableTask> tasks = new ArrayList<>(2);
-    tasks.add(buildStagingJobs(numberOfStagingJobs, tenant));
-    tasks.add(new StringTask("writing-job tenant-" + tenant));
-    return new SerialTaskContainer(tasks);
-  }
-
-  private RecoverableTask buildStagingJobs(int numberOfTasks, int tenant) {
-    Collection<RecoverableTask> tasks = new ArrayList<>(numberOfTasks);
-    for (int i = 0; i < numberOfTasks; i++) {
-      tasks.add(new StringTask("staging-job-" + i + " tenant-" + tenant));
-    }
-    return new ParallelTaskContainer(tasks);
+    this.pool.invoke(JobTrees.buildTree());
   }
 
   enum NullListener implements TaskStateListener {
@@ -127,21 +56,6 @@ public class PunchTest {
 
     private void print(TaskPath path, String whatHappened) {
       System.out.println("task " + path + " " + whatHappened);
-    }
-
-  }
-
-  static final class StringTask extends ListenableTask {
-
-    private final String s;
-
-    StringTask(String s) {
-      this.s = s;
-    }
-
-    @Override
-    protected void safeCompute() {
-      System.out.println(this.s);
     }
 
   }
