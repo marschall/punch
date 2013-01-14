c807d396ded4265c7324a0eee4f83000ef49ee46
Philippe Marschall <pm@netcetera.ch>
2013-01-14 14:38:16 +0100

    add transaction handling

--- src/test/java/com/github/marschall/punch/DatabaseTest.java
+++ src/test/java/com/github/marschall/punch/DatabaseTest.java
@@ -5,6 +5,8 @@ import java.sql.SQLException;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicReference;
 
@@ -13,9 +15,14 @@ import org.junit.Before;
 import org.junit.Test;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import static org.junit.Assert.assertEquals;
 
@@ -34,7 +41,8 @@ public class DatabaseTest {
     .build();
 
     this.jdbcTemplate = new JdbcTemplate(this.db);
-    this.pool = new PunchPool(new PersistingTaskStateListener(this.jdbcTemplate));
+    PlatformTransactionManager transactionManager = new DataSourceTransactionManager(this.db);
+    this.pool = new PunchPool(new PersistingTaskStateListener(this.jdbcTemplate, transactionManager));
   }
 
   @After
@@ -95,24 +103,35 @@ public class DatabaseTest {
     private static final String UPDATE_TASK_SQL = "UPDATE t_task_state SET task_state = ? WHERE task_path = ?";
 
     private final JdbcTemplate jdbcTemplate;
+    private final PlatformTransactionManager transactionManager;
+    private final ConcurrentMap<TaskPath, TransactionStatus> transactionStates;
+    private final TransactionDefinition transactionDefinition;
 
-    public PersistingTaskStateListener(JdbcTemplate jdbcTemplate) {
+    PersistingTaskStateListener(JdbcTemplate jdbcTemplate, PlatformTransactionManager transactionManager) {
       this.jdbcTemplate = jdbcTemplate;
+      this.transactionManager = transactionManager;
+      this.transactionStates = new ConcurrentHashMap<>();
+      this.transactionDefinition = new DefaultTransactionDefinition();
     }
 
     @Override
     public void taskStarted(TaskPath path) {
+      TransactionStatus status = this.transactionManager.getTransaction(this.transactionDefinition);
+      this.transactionStates.put(path, status);
       this.jdbcTemplate.update(INSERT_TASK_SQL, path.toString(), "RUNNING");
     }
 
     @Override
     public void taskFinished(TaskPath path) {
       this.jdbcTemplate.update(UPDATE_TASK_SQL, "FINISHED", path.toString());
+      TransactionStatus status = this.transactionStates.remove(path);
+      this.transactionManager.commit(status);
     }
 
     @Override
     public void taskFailed(TaskPath path) {
-      // nop
+      TransactionStatus status = this.transactionStates.remove(path);
+      this.transactionManager.rollback(status);
     }
   }
 
