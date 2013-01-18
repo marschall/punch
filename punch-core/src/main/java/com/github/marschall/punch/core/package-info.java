96cf40cd6ac0b67799b456d8f1e37bd5d80d64eb
Philippe Marschall <pm@netcetera.ch>
2013-01-18 12:47:30 +0100

    update doc

--- punch-core/src/main/java/com/github/marschall/punch/core/package-info.java
+++ punch-core/src/main/java/com/github/marschall/punch/core/package-info.java
@@ -2,7 +2,7 @@ package com.github.marschall.punch.core;
 
 /**
  * The absolute minimum core classes.
- * 
+ *
  * Concepts:
  *  - a VM crash is treated like a termination due to an exception
  *  - the state of a task is either
@@ -12,12 +12,11 @@ package com.github.marschall.punch.core;
  *  - in a case of a re-run all not finished tasks are run again
  *  - a task is identified by a path
  *  - in a recovery case the job tree looks exactly the same
- * 
+ *
  * Things to consider:
  *  - drop "running" state
- * 
+ *
  * Possible features:
- *  - composite task listener
  *  - idempotent tasks that don't require a transaction
  *  - #getDescription() on listanble task
  *  - pass task to listener
